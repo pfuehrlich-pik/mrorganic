4b0778f1672d5f6c84c7944c1305da5d65ffb207
Jan Philipp Dietrich <dietrich@pik-potsdam.de>
2023-01-06 16:19:47 +0100

    tweaks

--- R/calcSOCbyLandType.R
+++ R/calcSOCbyLandType.R
@@ -27,34 +27,35 @@ calcSOCbyLandType <- function() {
   # read in aggregated area information and setting zeroValue to 10^-10 to ensure
   # that all values are > 0 so that it can be used as weight
   weight <- calcOutput("LandTypeAreasAggregated", res = res, zeroValue = 10^-10, aggregate = FALSE)
+  message("Relevant data read in. (1/6)")
 
-  message("Relevant data read in. (1/5)")
+  # align data sets by projecting soc onto weight and masking weight onto soc
+  soc    <- terra::project(soc, weight)
+  weight <- terra::mask(weight, soc)
+  message("Aligning data sets (2/6)")
 
   # aggregate weight to intermediate target resolution
   # (for further processing to happen in memory)
-  weight25 <- terra::aggregate(weight, fact = 30, fun = "sum")
-  message("Land use aggregation to 0.25deg completed. (2/5)")
+  weight25 <- terra::aggregate(weight, fact = 30, fun = "sum", na.rm=TRUE)
+  message("Land use aggregation to 0.25deg completed. (3/6)")
 
   # area weighted aggregation of SOC to 0.25 degree
-  soc   <- terra::project(soc, weight)
-  soc25 <- terra::aggregate(soc * weight, fact = 30, fun = "sum") / weight25
-  message("Area weighted SOC aggregation to 0.25deg completed. (3/5)")
+  soc25 <- terra::aggregate(soc * weight, fact = 30, fun = "sum", na.rm = TRUE) / weight25
+  message("Area weighted SOC aggregation to 0.25deg completed. (4/6)")
 
   # convert to magpie objects and clean coordinate values
   x <- as.magpie(soc25)
   getCoords(x) <- round(getCoords(x), 2)
   weight <- as.magpie(weight25)
   getCoords(weight) <- round(getCoords(weight), 2)
-  # reduce weight to cells also available in x
-  weight <- weight[getItems(x, dim = 1), , ]
-  message("Conversion to magclass completed. (4/5)")
+  message("Conversion to magclass completed. (5/6)")
 
   # add country ISO codes
   worldCountries <- calcOutput("WorldCountries", aggregate = FALSE)
   countryCodes <- terra::extract(worldCountries, getCoords(x))$value
   getItems(x, dim = "country", maindim = 1)      <- countryCodes
   getItems(weight, dim = "country", maindim = 1) <- countryCodes
-  message("Country codes added. (5/5)")
+  message("Country codes added. (6/6)")
 
   return(list(x = x,
               weight = weight,
