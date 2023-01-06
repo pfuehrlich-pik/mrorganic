25061b74fa48a71aff7f4973e983645cdf6e2a0e
Jan Philipp Dietrich <dietrich@pik-potsdam.de>
2023-01-06 11:31:23 +0100

    split calculations for improved data caching and fixed some bugs

---
+++ R/calcLandTypeAreasAggregated.R
@@ -0,0 +1,37 @@
+#' Aggregate land type areas
+#'
+#' Aggregate land type area information to a desired resolution
+#'
+#' @param res target resolution
+#' @param zeroValue value for cells with no area. Useful to change to a very
+#' small number if the area information should be used as weight.
+#' @return data
+#'
+#' @author Jan Philipp Dietrich
+#' @examples
+#' \dontrun{
+#' a <- calcOutput("LandTypeAreas")
+#' }
+#' @seealso \code{\link{calcOutput}}
+
+calcLandTypeAreasAggregated <- function(res, zeroValue = 0) {
+  terra::terraOptions(tempdir = getConfig("tmpfolder"))
+  message("Please be patient, this will take now a while.")
+
+  # read in area information
+  x <- calcOutput("LandTypeAreas", aggregate = FALSE)
+
+  # round to fix floating point issues
+  fact <- round(res / terra::res(x), 2)
+
+  # check if fact is an integer value
+  if(!identical(round(fact), fact)) stop("Resolution must be a integer multiple of ", terra::res(x)[1])
+
+  # aggregate area information to SOC resolution and add very small area to each cell
+  # ensuring that all values are > 0 so that it can be used as weight
+  out <- terra::aggregate(x, fact = fact, fun = "sum") +  zeroValue
+  return(list(x = out,
+              description = "Estimated, aggregated area for cropland, grassland and other types based on ESACCI data.",
+              unit = "ha",
+              class = "SpatRaster"))
+}
--- R/calcSOCbyLandType.R
+++ R/calcSOCbyLandType.R
@@ -20,28 +20,41 @@ calcSOCbyLandType <- function() {
   # read in SOC data
   soc <- readSource("GSOCseq", subtype = "ini", convert = FALSE)
 
-  # read in area information
-  l <- calcOutput("LandTypeAreas", aggregate = FALSE)
+  # get soc resolution. Rounding is performed to fix a representation problem
+  # of the float value
+  res <- round(terra::res(soc)[1], 9)
 
-  # aggregate area information to SOC resolution and add very small area to each cell
-  # ensuring that all values are > 0 so that it can be used as weight
-  weight <- terra::aggregate(l, fact = terra::res(soc) / terra::res(l), fun = "sum") +  10^-10
+  # read in aggregated area information and setting zeroValue to 10^-10 to ensure
+  # that all values are > 0 so that it can be used as weight
+  weight <- calcOutput("LandTypeAreasAggregated", res = res, zeroValue = 10^-10, aggregate = FALSE)
+
+  message("Relevant data read in. (1/5)")
 
   # aggregate weight to intermediate target resolution
   # (for further processing to happen in memory)
   weight25 <- terra::aggregate(weight, fact = 30, fun = "sum")
+  message("Land use aggregation to 0.25deg completed. (2/5)")
 
   # area weighted aggregation of SOC to 0.25 degree
   soc   <- terra::project(soc, weight)
   soc25 <- terra::aggregate(soc * weight, fact = 30, fun = "sum") / weight25
+  message("Area weighted SOC aggregation to 0.25deg completed. (3/5)")
+
+  # convert to magpie objects and clean coordinate values
+  x <- as.magpie(soc25)
+  getCoords(x) <- round(getCoords(x), 2)
+  weight <- as.magpie(weight25)
+  getCoords(weight) <- round(getCoords(weight), 2)
+  # reduce weight to cells also available in x
+  weight <- weight[getItems(x, dim = 1), , ]
+  message("Conversion to magclass completed. (4/5)")
 
   # add country ISO codes
-  countryCodes <- calcOutput("WorldCountries", aggregate = FALSE)
-
-  x <- as.magpie(soc25)
-  getItems(x, dim = "country", maindim = 1) <- terra::extract(countryCodes, getCoords(x))$value
-  weight <- as.magpie(weight25)
-  getItems(weight, dim = "country", maindim = 1) <- terra::extract(countryCodes, getCoords(weight))$value
+  worldCountries <- calcOutput("WorldCountries", aggregate = FALSE)
+  countryCodes <- terra::extract(worldCountries, getCoords(x))$value
+  getItems(x, dim = "country", maindim = 1)      <- countryCodes
+  getItems(weight, dim = "country", maindim = 1) <- countryCodes
+  message("Country codes added. (5/5)")
 
   return(list(x = x,
               weight = weight,
