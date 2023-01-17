a1e19e7516f8f44f7141033d1ac38308e1ef709e
Pascal Führlich <pascal.fuehrlich@pik-potsdam.de>
2023-01-17 15:37:13 +0100

    re-enable downloadGADM output

--- R/downloadGADM.R
+++ R/downloadGADM.R
@@ -2,9 +2,7 @@ downloadGADM <- function() {
   isoCodes <- c("ALA", "ATA", "BES", "BVT", "CCK", "CXR", "ESH", "GLP",
                 "GUF", "MTQ", "MYT", "REU", "SJM", "TKL", "TWN")
   writeLines(isoCodes, "isoCodes.txt")
-  message("Downloading GADM data for the following countries:\n", paste(isoCodes, collapse = ", "),
-          "\nThis will take a couple minutes.") # TODO alternatively omit quiet = TRUE so user will know what's downloading
-  geodata::gadm(isoCodes, 0, ".", version = "4.1", resolution = 2, quiet = TRUE)
+  geodata::gadm(isoCodes, 0, ".", version = "4.1", resolution = 2)
   return(list(url          = "https://gadm.org/data.html",
               doi          = NULL,
               title        = "GADM data",
