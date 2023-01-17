b57045a41abfdde3cd875a90ac2a96acb6f003f6
Pascal Führlich <pascal.fuehrlich@pik-potsdam.de>
2023-01-17 15:43:13 +0100

    author GADM

--- R/downloadGADM.R
+++ R/downloadGADM.R
@@ -1,14 +1,16 @@
 downloadGADM <- function() {
-  isoCodes <- c("ALA", "ATA", "BES", "BVT", "CCK", "CXR", "ESH", "GLP",
-                "GUF", "MTQ", "MYT", "REU", "SJM", "TKL", "TWN")
-  writeLines(isoCodes, "isoCodes.txt")
-  geodata::gadm(isoCodes, 0, ".", version = "4.1", resolution = 2)
+  countryCodes <- c("ALA", "ATA", "BES", "BVT", "CCK", "CXR", "ESH", "GLP",
+                    "GUF", "MTQ", "MYT", "REU", "SJM", "TKL", "TWN")
+  writeLines(countryCodes, "countryCodes.txt")
+
+  geodata::gadm(countryCodes, 0, ".", version = "4.1", resolution = 2)
+
   return(list(url          = "https://gadm.org/data.html",
               doi          = NULL,
               title        = "GADM data",
               description  = paste("GADM wants to map the administrative areas of all countries, ",
                                    "at all levels of sub-division."),
-              author       = "World Bank",
+              author       = "GADM",
               unit         = "1",
               version      = "4.1",
               release_date = "2022-07-16",
