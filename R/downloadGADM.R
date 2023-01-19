78ff13fbf4646c7b1c8de78f1daf3c473c4f3d7e
Pascal Führlich <pascal.fuehrlich@pik-potsdam.de>
2023-01-19 11:14:30 +0100

    get all countries from GADM

--- R/downloadGADM.R
+++ R/downloadGADM.R
@@ -1,9 +1,6 @@
 downloadGADM <- function() {
-  countryCodes <- c("ALA", "ATA", "BES", "BVT", "CCK", "CXR", "ESH", "GLP",
-                    "GUF", "MTQ", "MYT", "REU", "SJM", "TKL", "TWN")
-  writeLines(countryCodes, "countryCodes.txt")
-
-  geodata::gadm(countryCodes, 0, ".", version = "4.1", resolution = 2)
+  # TODO parallel downloads
+  geodata::gadm(madrat::getISOlist(), 0, ".", version = "4.1", resolution = 2)
 
   return(list(url          = "https://gadm.org/data.html",
               doi          = NULL,
--- R/readGADM.R
+++ R/readGADM.R
@@ -1,4 +1,4 @@
 readGADM <- function() {
-  x <- geodata::gadm(readLines("countryCodes.txt"), 0, ".", version = "4.1", resolution = 2)
+  x <- geodata::gadm(madrat::getISOlist(), 0, ".", version = "4.1", resolution = 2)
   return(list(x = x, class = "SpatVector", cache = FALSE))
 }
