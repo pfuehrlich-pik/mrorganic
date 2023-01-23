4d789e8cca8c7ef4d909b04ca1ab488f57e423d8
Jan Philipp Dietrich <dietrich@pik-potsdam.de>
2023-01-23 16:18:26 +0100

    small fix

--- R/calcSOCbyLandType.R
+++ R/calcSOCbyLandType.R
@@ -26,22 +26,22 @@ calcSOCbyLandType <- function() {
   # read in aggregated area information and setting zeroValue to 10^-10 to ensure
   # that all values are > 0 so that it can be used as weight
   weight <- calcOutput("LandTypeAreasAggregated", res = res, zeroValue = 10^-10, aggregate = FALSE)
-  message("Relevant data read in. (1/6)")
+  message("Relevant data read in. (1/5)")
 
   # align data sets by projecting soc onto weight and masking weight onto soc
   soc    <- terra::project(soc, weight)
   weight <- terra::mask(weight, soc)
-  message("Aligning data sets (2/6)")
+  message("Aligning data sets (2/5)")
 
   # aggregate weight to intermediate target resolution
   # (for further processing to happen in memory)
   weight25 <- terra::aggregate(weight, fact = 30, fun = "sum", na.rm = TRUE)
-  message("Land use aggregation to 0.25deg completed. (3/6)")
+  message("Land use aggregation to 0.25deg completed. (3/5)")
 
   # area weighted aggregation of SOC to 0.25 degree
   soc25 <- terra::aggregate(soc * weight, fact = 30, fun = "sum", na.rm = TRUE) / weight25
   names(soc25) <- names(weight25)
-  message("Area weighted SOC aggregation to 0.25deg completed. (4/6)")
+  message("Area weighted SOC aggregation to 0.25deg completed. (4/5)")
 
   # convert to magpie objects and clean coordinate values
   .clean2magpie <- function(x) {
@@ -97,9 +97,7 @@ calcSOCbyLandType <- function() {
   template <- .getCountryTemplate()
   x        <- .convert2magpie(soc25, template, missing = 0)
   weight   <- .convert2magpie(weight25, template, missing = 10^-10, silent = TRUE)
-  message("Conversion to magclass completed. (5/6)")
-
-  #message("Filling of gaps completed. (6/6)")
+  message("Conversion to magclass completed. (5/5)")
 
   return(list(x = x,
               weight = weight,
