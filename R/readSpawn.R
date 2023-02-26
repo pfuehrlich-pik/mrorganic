60f576f5bb533cf252b89f552147ba77eaca7400
Jan Philipp Dietrich <dietrich@pik-potsdam.de>
2023-02-26 19:20:44 +0100

    added readSpawn

---
+++ R/readSpawn.R
@@ -0,0 +1,26 @@
+#' Read Spawn
+#'
+#' Read Spawn data
+#'
+#' @param subtype Subtype to be read in. Available types are
+#' "abovegroundBiomass", "abovegroundBiomassUncertainty",
+#' "belowgroundBiomass" and "belowgroundBiomassUncertainty".
+#' @return data
+#'
+#' @author Jan Philipp Dietrich
+#' @examples
+#' \dontrun{
+#' a <- readSource("Spawn", "abovegroundBiomass")
+#' }
+#' @seealso \code{\link{readSource}}
+
+readSpawn <- function(subtype) {
+  f <- toolSubtypeSelect(subtype,
+        c(abovegroundBiomass = "aboveground_biomass_carbon_2010.tif",
+          abovegroundBiomassUncertainty = "aboveground_biomass_carbon_2010_uncertainty.tif",
+          belowgroundBiomass = "belowground_biomass_carbon_2010.tif",
+          belowgroundBiomassUncertainty = "belowground_biomass_carbon_2010_uncertainty.tif"))
+
+  r <- terra::rast(f)
+  return(list(x = r, class = "SpatRaster"))
+}
