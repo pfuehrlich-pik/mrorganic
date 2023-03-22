a9f8200a67351c26423f891241490dc3801cd63c
Pascal Führlich <pascal.fuehrlich@pik-potsdam.de>
2023-03-22 15:41:15 +0100

    calcEcoregions2017Raster returns datafra,e

--- R/calcEcoregions2017Raster.R
+++ R/calcEcoregions2017Raster.R
@@ -1,22 +1,25 @@
 #' calcEcoregions2017Raster
 #'
-#' Produces a raster with ecoregion information as a magclass object.
+#' Produces a raster with ecoregion information.
 #'
-#' @param ecoregionsColumn Which column of the ecoregions dataset should be in the raster.
 #' @param nrows Number of rows in the raster produced.
 #' @param ncols Number of columns in the raster produced.
-#' @return A raster with ecoregion information (ecoregionsColumn) as a magclass object
+#' @return A raster with ecoregion information as a data.frame
 #'
 #' @author Pascal Führlich
-calcEcoregions2017Raster <- function(ecoregionsColumn = "ECO_BIOME_", nrows = 720, ncols = 1440) {
-  stopifnot(length(ecoregionsColumn) == 1)
+calcEcoregions2017Raster <- function(nrows = 720, ncols = 1440) {
   ecoregions <- readSource("Ecoregions2017")
-  stopifnot(ecoregionsColumn %in% names(ecoregions))
-  x <- terra::rasterize(ecoregions, terra::rast(nrows = nrows, ncols = ncols), ecoregionsColumn,
-                        touches = TRUE, background = NA)
-  return(list(x = magclass::as.magpie(x),
-              description = paste0("A ", nrows, "x", ncols, " raster with ",
-                                   ecoregionsColumn, " data from Ecoregions 2017."),
+  ecoregionsRaster <- terra::rasterize(ecoregions, terra::rast(nrows = nrows, ncols = ncols), "OBJECTID",
+                                       touches = TRUE, background = NA)
+  x <- merge(terra::as.data.frame(ecoregionsRaster, xy = TRUE),
+             terra::as.data.frame(ecoregions), by = "OBJECTID")
+
+  # TODO the following check fails
+  # stopifnot(setequal(x$OBJECTID, c(ecoregions$OBJECTID, NA)))
+
+  return(list(x = x,
+              description = paste0("A ", nrows, "x", ncols, " raster with data from Ecoregions 2017."),
               unit = "categorical",
+              class = "data.frame",
               isocountries = FALSE))
 }
